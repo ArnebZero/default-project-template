1348cf3350cbe784b0d2b56b00c217b2141a480a
ArnebZero <yargreb@yandex.ru>
2025-04-17 17:18:45 +0300

    add docstring for file removing functionality

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -3,6 +3,12 @@ from pathlib import Path
 
 
 def remove_file_or_folder(path: str | Path) -> None:
+    """Remove a file or folder at the specified path.
+    If the path is a directory, it will be removed recursively.
+    
+    Args:
+        path (str | Path): The path to the file or folder to remove.
+    """
     path = Path(path)
     if not path.exists():
         return
